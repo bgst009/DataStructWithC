72e7fc27b65ec6a57d1269c341afa6ae77e37f39
bgst009 <1499135121@qq.com>
2020-09-10 14:42:18 +0800

    Dynamic Array test done

--- Array/DynamicArray/Array.c
+++ Array/DynamicArray/Array.c
@@ -12,50 +12,144 @@
 
 struct MyArray {
     ElementType* data;
-    int size;
-    int number;
+    //数组容量
+    int capacity;
+    //元素个数
+    int length;
 };
-Array initArray(int size) {
+Array initArray(int capacity) {
     Array array;
-    array.data = (ElementType*) malloc(sizeof(ElementType) * size);
-    memset(array.data, 0, sizeof(ElementType) * size);
-    array.size = size;
-    array.number = 0;
+    //分配内存
+    array.data = (ElementType*) malloc(sizeof(ElementType) * capacity);
+    //初始化
+    memset(array.data, 0, sizeof(ElementType) * capacity);
+    array.capacity = capacity;
+    array.length = 0;
     return array;
 }
-void getElement(Array array, int number, ElementType* element) {
-    if (number > array.number)
+void getElement(Array array, int index, ElementType* element) {
+    if (index > array.length)
         return;
-    *element = array.data[number];
+    *element = array.data[index];
 }
-void addElement(Array* array, ElementType element) {
-    if (array->number < array->size) {
-        array->data[array->number++] = element;
-    } else {
+
+void setElement(Array* array, int index, ElementType element) {
+    if (index > array->length)
+        return;
+    array->data[index] = element;
+}
+void addElement(Array* array, int index, ElementType element) {
+    //参数不合法
+    if (index < 0 && index > array->length) {
+        printf("\n Add failed. Require index >= 0 and index <= size. \n");
+        return;
+    }
+
+    //    printf("%d ", element);
+
+    //容量够直接赋值
+    if (array->length < array->capacity) {
+        for (int i = array->length - 1; i >= index; i--) {
+            array->data[i + 1] = array->data[i];
+        }
+        array->data[index] = element;
+        array->length++;
+    } else {//不够在分配内存
         array->data = (ElementType*) realloc(array->data, sizeof(ElementType) * INCREMENT);
-        array->data[array->number++] = element;
-        array->size += INCREMENT;
+        for (int i = array->length - 1; i >= index; i--) {
+            array->data[i + 1] = array->data[i];
+        }
+        array->data[index] = element;
+        array->capacity += INCREMENT;
+        array->length++;
     }
 }
-void traverseArray(Array array) {
-    printf("\n");
-    for (int i = 0; i < array.number; ++i) {
-        printf(" %d ", array.data[i]);
+void addLast(Array* array, ElementType element) {
+    addElement(array, array->length, element);
+}
+void addFirst(Array* array, ElementType element) {
+    addElement(array, 0, element);
+}
+void printArray(Array array) {
+    int i;
+    printf("\n [");
+    for (i = 0; i < array.length - 1; i++) {
+        printf("%d , ", array.data[i]);
     }
+    printf("%d]", array.data[i]);
     printf("\n");
+}
+int getCapacity(Array array) {
+    return array.capacity;
+}
+int getLength(Array array) {
+    return array.length;
+}
+bool isEmpty(Array array) {
+    return array.length == 0 ? true : false;
+}
+bool containsElement(Array array, ElementType element) {
+    for (int i = 0; i < array.length; ++i) {
+        if (array.data[i] == element)
+            return true;
+    }
+    return false;
+}
+int findElement(Array array, ElementType element) {
+    for (int i = 0; i < array.length; ++i) {
+        if (array.data[i] == element) {
+            //            printf("%d", i);
+            return i;
+        }
+    }
+    return -1;
+}
+int removeElementWithIndex(Array* array, int index) {
+    //参数不合法
+    if (index < 0 && index >= array->length) {
+        printf("\n Remove failed. Index is illegal. \n");
+        return -1;
+    }
+    int ret = array->data[index];
+    for (int i = index + 1; i < array->length; i++) {
+        array->data[i - 1] = array->data[i];
+    }
+    array->length--;
+    return ret;
+}
+int removeFirst(Array* array, int index) {
+    removeElementWithIndex(array, 0);
+}
+int removeLast(Array* array, int index) {
+    removeElementWithIndex(array, array->length - 1);
+}
+void removeElement(Array* array, ElementType element) {
+    int index = findElement(*array, element);
+    if (index != -1) {
+        removeElementWithIndex(array, index);
+    }
 }
 
 int main() {
 
-    int size = 0;
+    int size = 5;
     Array array = initArray(size);
 
-    srand(time(NULL));
+    //    srand(time(NULL));
     for (int i = 0; i < size + 5; ++i) {
-        addElement(&array, rand() % 100 + 1);
+        addLast(&array, i);
     }
+    printArray(array);
 
-    traverseArray(array);
+    addFirst(&array, 10);
+    printArray(array);
+    addLast(&array, 11);
+    printArray(array);
+
+    removeElementWithIndex(&array, 2);
+    printArray(array);
+    removeElement(&array, 4);
+    printArray(array);
 
 
     return 0;
--- Array/DynamicArray/Array.h
+++ Array/DynamicArray/Array.h
@@ -4,15 +4,102 @@
 
 #ifndef DATASTRUCTWITHC_ARRAY_H
 #define DATASTRUCTWITHC_ARRAY_H
+#include <stdbool.h>
 #define INCREMENT 20
 
 typedef struct MyArray Array;
 typedef int ElementType;
 
 
-Array initArray(int size);
-void getElement(Array array, int number, ElementType* element);
-void addElement(Array* array, ElementType element);
+/**
+ * 初始化固定大小的数组
+ * @param capacity
+ * @return 数组
+ */
+Array initArray(int capacity);
+/**
+ * 获取索引位置的元素
+ * @param array 数组
+ * @param index 索引
+ * @param element 元素指针
+ */
+void getElement(Array array, int index, ElementType* element);
+/**
+ * 修改index位置的元素
+ * @param array pointer to array
+ * @param index 索引
+ * @param element 元素
+ */
+void setElement(Array* array, int index, ElementType element);
+/**
+ * 在索引位置处添加一个元素
+ * @param array 数组指针
+ * @param index 索引
+ * @param element 元素
+ */
+void addElement(Array* array, int index, ElementType element);
+/**
+ * 在所有元素后添加一个元素
+ * @param array 数组指针
+ * @param element 元素
+ */
+void addLast(Array* array, ElementType element);
+/**
+ * 在所有元素前添加一个元素
+ * @param array 数组指针
+ * @param element 元素
+ */
+void addFirst(Array* array, ElementType element);
+
+/**
+ * 获取数组的容量
+ * @param array 数组
+ * @return 容量
+ */
+int getCapacity(Array array);
+/**
+ * 获取数组中的元素个数
+ * @param array 数组
+ * @return 个数
+ */
+int getLength(Array array);
+/**
+ * 返回数组是否为空
+ * @param array 数组
+ * @return if empty array return true else return false
+ */
+bool isEmpty(Array array);
+
+/**
+ * 查找数组中是否有元素element
+ * @param array 数组
+ * @param element 元素
+ * @return
+ */
+bool containsElement(Array array, ElementType element);
+
+/**
+ * 查找数组中元素element所在的索引，如果不存在元素e，则返回-1
+ * @param array 数组
+ * @param element 元素
+ * @return index of element in array
+ */
+int findElement(Array array, ElementType element);
+
+/**
+ * 从数组中删除index位置的元素, 返回删除的元素
+ * @param array
+ * @param index
+ * @return
+ */
+int removeElementWithIndex(Array* array, int index);
+
+int removeFirst(Array* array, int index);
+int removeLast(Array* array, int index);
+void removeElement(Array* array, ElementType element);
+
+
+void printArray(Array array);
 
 
 #endif//DATASTRUCTWITHC_ARRAY_H
